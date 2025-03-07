39eeeb1cf5761c0cc93b5e93da954121ff0a1fb3
sri harish <sriharish9626@gmail.com>
2025-03-07 18:31:04 +0530

    Investment calculator using functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/profit
+
+go 1.23.5
---
+++ using functions.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxrate float64
+
+	//fmt.Print("Revenue: ")
+	output("Revenue: ")
+	fmt.Scan(&revenue)
+	//fmt.Print("Expenses: ")
+	output("expenses: ")
+	fmt.Scan(&expenses)
+	//fmt.Print("Tax Rate: ")
+	output("TaxRate: ")
+	fmt.Scan(&taxrate)
+
+	Ebt, profit, ratio := profitcalculation(revenue, expenses, taxrate)
+	//Ebt := revenue - expenses
+	//profit := Ebt * (1 - taxrate/100)
+	//ratio := Ebt / profit
+	fmt.Println("Earning Before Tax :", Ebt)
+	fmt.Printf("Profit:%.2f\n", profit)
+	fmt.Printf("Ratio:%.2f\n", ratio)
+
+}
+func output(text string) {
+	fmt.Print(text)
+
+}
+func profitcalculation(revenue, expenses, taxrate float64) (Ebt1 float64, profit1 float64, ratio1 float64) {
+	Ebt1 = revenue - expenses
+	profit1 = Ebt1 * (1 - taxrate/100)
+	ratio1 = Ebt1 / profit1
+	return Ebt1, profit1, ratio1
+}
