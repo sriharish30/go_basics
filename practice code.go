1c2b351086a8fb7722c93eed18a855b11696ffbe
sri harish <sriharish9626@gmail.com>
2025-05-31 17:07:28 +0530

    JSON files after changes in the packages and main file.go

--- practice code.go
+++ practice code.go
@@ -14,3 +14,34 @@ func main() {
 	}
 
 }
+//after using the cmdmanager package and iomanager package the program will look like this 
+
+
+package main
+
+import (
+	"example/practice/filehandling"
+	"example/practice/prices"
+	"fmt"
+)
+
+func main() {
+
+	Taxrate := []float64{0, 0.07, 0.1, 0.15}
+
+	for _, rate := range Taxrate {
+		fh := filehandling.New("prices.txt", fmt.Sprintf("result_%.0f.json", rate*100))
+		//cmdm := cmdmanager.New()
+		JobPrice := prices.NewTaxIncludedPriceJob(fh, rate)
+		err := JobPrice.Process()
+		if err != nil {
+			fmt.Println("could not process job")
+			fmt.Println(err)
+		}
+	}
+
+}
+
+
+
+
