03324a3834c544ab90e3133fbdf32520b760600a
sri harish <sriharish9626@gmail.com>
2025-05-31 17:06:02 +0530

    File handling in prices.go file after some changes

---
+++ filehandling/filehandling 1.go
@@ -0,0 +1,59 @@
+package filehandling
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"os"
+)
+
+type FileHandling struct {
+	InputFilePath  string
+	OutputFilePath string
+}
+
+func (fh FileHandling) ReadLines() ([]string, error) {
+	file, err := os.Open(fh.InputFilePath)
+	if err != nil {
+		return nil, errors.New("Failed to open file ")
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		file.Close()
+		return nil, errors.New("Failed to read line in the file ")
+	}
+	file.Close()
+	return lines, nil
+}
+func (fh FileHandling) WriteResult(data any) error {
+	file, err := os.Create(fh.OutputFilePath)
+	if err != nil {
+		return errors.New("Failed to create a file")
+	}
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data)
+	if err != nil {
+		file.Close()
+		return errors.New("Failed to convert data to JSON")
+	}
+	file.Close()
+	return nil
+
+}
+func New(inputpath, outputPath string) FileHandling {
+	return FileHandling{
+		InputFilePath:  inputpath,
+		OutputFilePath: outputPath,
+	}
+
+}
