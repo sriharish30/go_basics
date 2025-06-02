907f5b874690c69e811c340992d06e89373a329f
sri harish <sriharish9626@gmail.com>
2025-06-02 16:41:33 +0530

    this is th main function of the program that include go Routines

--- practice code.go
+++ practice code.go
@@ -42,6 +42,38 @@ func main() {
 
 }
 
+// main function code after using the go Routines 
+package main
+
+import (
+	"example/practice/filehandling"
+	"example/practice/prices"
+	"fmt"
+)
+
+func main() {
+	donechan := make([]chan bool, 4)
+
+	Taxrate := []float64{0, 0.07, 0.1, 0.15}
+
+	for index, rate := range Taxrate {
+		donechan[index] = make(chan bool)
+		fh := filehandling.New("prices.txt", fmt.Sprintf("result_%.0f.json", rate*100))
+		//cmdm := cmdmanager.New()
+		JobPrice := prices.NewTaxIncludedPriceJob(fh, rate)
+		go JobPrice.Process(donechan[index])
+		//if err != nil {
+		//	fmt.Println("could not process job")
+		//	fmt.Println(err)
+		//}
+	}
+	for _, donechans := range donechan {
+		<-donechans
+	}
+
+}
+
+
 
 
 
